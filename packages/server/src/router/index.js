2eceea27aacd7c62b699d32cc384e53ab32ad336
poplark <songkang666@126.com>
2022-03-18 18:17:08 +0800

    feat: server - 嵌套路由

--- packages/server/src/router/index.js
+++ packages/server/src/router/index.js
@@ -3,6 +3,13 @@ const init = require('./init');
 
 const router = new Router();
 
-init(router);
+// 在 Router 上使用中间件，来 嵌套路由
+const initRouter = new Router({
+  prefix: '/init-test'
+});
+
+init(initRouter);
+
+router.use(initRouter.routes());
 
 module.exports = router;
