f3a1bce72fd0f233117e028d5815b389bbe8e77a
poplark <songkang666@126.com>
2022-03-30 10:47:50 +0800

    feat: 加载 elemnent-plus
    affects: @f2f/client

--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import Layout from './Layout.vue'
 import { router } from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 const app = createApp(Layout);
 app.use(router);
+app.use(ElementPlus);
 
 app.mount('#app');
