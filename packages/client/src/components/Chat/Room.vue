ab56224b6db319ce53b862ab42904746b0565098
poplark <songkang666@126.com>
2022-04-08 15:24:41 +0800

    feat: 聊天消息样式
    affects: @f2f/client

--- packages/client/src/components/Chat/Room.vue
+++ packages/client/src/components/Chat/Room.vue
@@ -1,29 +1,35 @@
 <template>
 <div class="chat-room">
   <div v-if="state.chat">
-    <div >
-      <h2>消息</h2>
-      <div v-for="(message, index) in state.chat.messages" :key="index">
-        [{{message.username}}@{{formatTimestamp(message.timestamp)}}]: {{message.content}}
-      </div>
-    </div>
-    <div>
-      <h2>用户</h2>
-      <div @click="onChooseUser(user)" v-for="user in state.chat.users" :key="user.id">
-        {{user.username}}
-      </div>
-    </div>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleSwitchTab">
+      <el-tab-pane label="用户列表" name="first">
+        <div @click="onChooseUser(user)" v-for="user in state.chat.users" :key="user.id">
+          {{user.username}}
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="消息" name="second">
+        <div :class="computeClass(state.user, message)" v-for="(message, index) in state.chat.messages" :key="index">
+          <div class="user">
+            <span time="username">{{message.username}}</span>
+            <span class="time">{{formatTimestamp(message.timestamp)}}</span>
+          </div>
+          <div class="content">
+            {{message.content}}
+          </div>
+        </div>
+        <el-input v-model="state.message" @keyup.enter="onSendMsg"></el-input>
+      </el-tab-pane>
+    </el-tabs>
   </div>
   <el-button @click="onJoin">join</el-button>
   <el-button @click="onLeave">leave</el-button>
-  <el-input v-model="state.message" @keyup.enter="onSendMsg"></el-input>
   <el-button @click="onSendMsg">send message</el-button>
   <el-button @click="onKickOut">kick out</el-button>
 </div>
 </template>
 
 <script>
-import { reactive, getCurrentInstance, onMounted, onUpdated, onBeforeUnmount } from 'vue';
+import { ref, reactive, getCurrentInstance, onMounted, onUpdated, onBeforeUnmount } from 'vue';
 import { createInstance } from '@f2f/im-client';
 
 export default {
@@ -40,6 +46,11 @@ export default {
       message: ''
     });
     let {ctx:internalInstance} = getCurrentInstance();
+
+    const activeName = ref('first')
+    const handleSwitchTab = (tab, event) => {
+      console.log(tab, event)
+    }
 
     function onJoin() {
       const { user, chat } = state;
@@ -91,8 +102,17 @@ export default {
       state.choose = user;
     }
 
+    function computeClass(user, message) {
+      return user.id === message.userId ? 'message myself' : 'message';
+    }
     function formatTimestamp(timestamp) {
-      return new Date(timestamp);
+      const now = new Date();
+      const time = new Date(timestamp);
+      if (now.toDateString() !== time.toDateString()) {
+        return time.toLocaleString();
+      } else {
+        return time.toLocaleTimeString();
+      }
     }
 
     onMounted(() => {
@@ -106,27 +126,52 @@ export default {
     });
 
     return {
+      activeName,
+      handleSwitchTab,
       state,
       onJoin,
       onLeave,
       onSendMsg,
       onKickOut,
       onChooseUser,
+      computeClass,
       formatTimestamp,
     }
   },
-  computed: {
-    users: function() {
-      console.log('computed::users::', this.state.chat.users);
-      this.state.chat.users;
-    },
-    messages: function() {
-      console.log('computed::messages::', this.state.chat.users);
-      this.state.chat.messages;
-    }
-  }
 }
 </script>
 
-<style>
+<style scoped>
+.chat-room {
+  max-width: 400px;
+}
+.message {
+  margin: 10px 0;
+}
+.message.myself {
+  text-align: right;
+}
+.message .user {
+  margin-bottom: 6px;
+  font-size: 12px;
+}
+.message .user .username {
+  color: #333;
+}
+.message .user .time {
+  margin-left: 6px;
+  color: #999;
+}
+.message .content {
+  display: inline-block;
+  padding: 6px 8px;
+  border: 1px;
+  border-radius: 6px;
+  background-color: #eee;
+  text-align: left;
+}
+.message.myself .content {
+  background-color: #A7E879;
+}
+
 </style>
