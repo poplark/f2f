a7b5e21aba50858be2aab2a81d7bcc28e969d00e
poplark <songkang666@126.com>
2022-04-08 13:50:54 +0800

    feat: uI 中实现基本功能
    affects: @f2f/client

--- packages/client/src/components/Chat/Room.vue
+++ packages/client/src/components/Chat/Room.vue
@@ -1,14 +1,29 @@
 <template>
 <div class="chat-room">
-  <span>{{state.user.username}}</span>
+  <div v-if="state.chat">
+    <div >
+      <h2>消息</h2>
+      <div v-for="(message, index) in state.chat.messages" :key="index">
+        [{{message.username}}@{{formatTimestamp(message.timestamp)}}]: {{message.content}}
+      </div>
+    </div>
+    <div>
+      <h2>用户</h2>
+      <div @click="onChooseUser(user)" v-for="user in state.chat.users" :key="user.id">
+        {{user.username}}
+      </div>
+    </div>
+  </div>
   <el-button @click="onJoin">join</el-button>
   <el-button @click="onLeave">leave</el-button>
+  <el-input v-model="state.message" @keyup.enter="onSendMsg"></el-input>
   <el-button @click="onSendMsg">send message</el-button>
+  <el-button @click="onKickOut">kick out</el-button>
 </div>
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue';
+import { reactive, getCurrentInstance, onMounted, onUpdated, onBeforeUnmount } from 'vue';
 import { createInstance } from '@f2f/im-client';
 
 export default {
@@ -21,14 +36,34 @@ export default {
     const state = reactive({
       user: props.user,
       chat: null,
+      choose: null,
+      message: ''
     });
+    let {ctx:internalInstance} = getCurrentInstance();
 
     function onJoin() {
-      const { user } = state;
+      const { user, chat } = state;
+      if (chat) return;
       state.chat = createInstance(user.id, user.username, 'token');
       console.log('chat:::: ', state.chat);
+      window.p = state.chat;
       state.chat.join('room-test').then((res) => {
         console.log('onJoin:::: ', res);
+      });
+      state.chat.on('user-online', (user) => {
+        console.log('on::user-online:::: ', user, internalInstance);
+        internalInstance.$forceUpdate();
+      });
+      state.chat.on('user-offline', (user) => {
+        console.log('on::user-offline:::: ', user);
+        internalInstance.$forceUpdate();
+      });
+      state.chat.on('ban', (reason) => {
+        console.log('on::ban:::: ', reason);
+      });
+      state.chat.on('message', (message) => {
+        console.log('on::message:::: ', message);
+        internalInstance.$forceUpdate();
       });
     }
     function onLeave() {
@@ -38,18 +73,58 @@ export default {
     }
 
     function onSendMsg() {
-      state.chat.sendMessage('hello f2f').then((res) => {
+      if (!state.message) return;
+      state.chat.sendMessage(state.message).then((res) => {
         console.log('onSendMsg:::: ', res);
+        state.message = '';
       });
     }
+
+    function onKickOut() {
+      if (!state.choose) return;
+      state.chat.kickOut(state.choose).then((res) => {
+        console.log('onKickOut:::: ', res);
+      });
+    }
+
+    function onChooseUser(user) {
+      state.choose = user;
+    }
+
+    function formatTimestamp(timestamp) {
+      return new Date(timestamp);
+    }
+
+    onMounted(() => {
+      console.log('onMounted::', state.user);
+    });
+    onUpdated(() => {
+      console.log('onUpdated::', state.user);
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount::', state.user);
+    });
 
     return {
       state,
       onJoin,
       onLeave,
       onSendMsg,
+      onKickOut,
+      onChooseUser,
+      formatTimestamp,
     }
   },
+  computed: {
+    users: function() {
+      console.log('computed::users::', this.state.chat.users);
+      this.state.chat.users;
+    },
+    messages: function() {
+      console.log('computed::messages::', this.state.chat.users);
+      this.state.chat.messages;
+    }
+  }
 }
 </script>
 
